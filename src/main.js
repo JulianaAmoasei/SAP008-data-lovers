69af7ebc5aa5cf3a6394759af184f5e0020f85d0
JulianaAmoasei <late.parrot@gmail.com>
2022-08-03 16:23:34 -0300

    add funcao de soma

--- src/main.js
+++ src/main.js
@@ -3,4 +3,10 @@ import { example } from './data.js';
 import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
+function soma(num) {
+  return num + num;
+}
+
+soma(1)
+
 console.log(example, data);
